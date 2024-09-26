7ab7415e54ef4815655923debf13f050be2dca65
xBruzx <alecszczechowicz@gmail.com>
2024-09-26 16:41:00 -0500

    uploading assignment

---
+++ main.cpp
@@ -0,0 +1,54 @@
+
+
+// TODO: INCLUDE LIBRARIES
+
+#include <iostream>
+using namespace std; 
+
+void printMenu(); 
+
+// TODO: CREATE FUNCTION PROTOTYPES
+
+
+int main() { 
+
+    // TODO: create our list object, specifying it handles Character objects.
+
+
+    // TODO: create two iterators
+
+
+    // input integer for our switch statement
+    int choice = 0; 
+
+    // TODO: load characters from file
+
+
+    // TODO: set traversal iterator to the beginning 
+    // set end "flag" iterator to end and decrement. 
+
+    // TODO: WHILE LOOP  & Switch Case
+    // print current character 
+    // take input
+    // switch case for input 
+   
+   // case 1 : next character 
+   // case 2 : prev character 
+   // case 3 : add character 
+   // case 4 : delete character
+   // case 5 : exit program 
+
+        
+
+    return 0;
+}
+
+void printMenu() { 
+    cout << "1. Next Character" << endl;
+    cout << "2. Previous Character" << endl;
+    cout << "3. Add Character" << endl; 
+    cout << "4. Delete Current Character" << endl; 
+    cout << "5. Exit Program" << endl; 
+}
+
+// TODO: MAKE YOUR FUNCTIONS GOOBER - NO! >:(
